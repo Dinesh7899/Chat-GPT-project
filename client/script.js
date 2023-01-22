228d03fdf2ec43f78d33c25bd931eb07b9628fdf
Dinesh7899 <96066586+Dinesh7899@users.noreply.github.com>
2023-01-22 13:48:48 +0530

    update

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://dinesh-8i9r.onrender.com/', {
+    const response = await fetch('https://dinesh-8i9r.onrender.com /', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
